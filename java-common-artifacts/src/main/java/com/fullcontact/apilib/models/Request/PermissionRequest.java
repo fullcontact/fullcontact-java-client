1cbc46623e40c5a02f2e87c7e3a9e99d5abffbda
Prakhar <47050738+prakhar-j@users.noreply.github.com>
2021-03-03 22:39:57 +0530

    Added Request models for Permission APIs

---
+++ java-common-artifacts/src/main/java/com/fullcontact/apilib/models/Request/PermissionRequest.java
@@ -0,0 +1,21 @@
+package com.fullcontact.apilib.models.Request;
+
+import com.fullcontact.apilib.FullContactException;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+
+@SuperBuilder(builderMethodName = "permissionRequestBuilder", toBuilder = true)
+@EqualsAndHashCode(callSuper = true)
+@Getter
+public class PermissionRequest extends PermissionRequestBasic {
+  private MultifieldRequest query;
+
+  public void validate() throws FullContactException {
+    super.validate();
+    if (this.getQuery() == null) {
+      throw new FullContactException("Query is required");
+    }
+    this.getQuery().validate();
+  }
+}
---
+++ java-common-artifacts/src/main/java/com/fullcontact/apilib/models/Request/PermissionRequestBasic.java
@@ -0,0 +1,54 @@
+package com.fullcontact.apilib.models.Request;
+
+import com.fullcontact.apilib.FullContactException;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.util.List;
+
+@SuperBuilder(toBuilder = true)
+@EqualsAndHashCode
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PermissionRequestBasic {
+  @Singular private List<PurposeRequest> consentPurposes;
+  private String locale;
+  private String ipAddress;
+  private String language;
+  private String collectionMethod;
+  private String collectionLocation;
+  private String policyUrl;
+  private String termsService;
+  private String tcf;
+  private Long timestamp;
+
+  protected static boolean isPopulated(String value) {
+    return value != null && !value.trim().isEmpty();
+  }
+
+  public void validate() throws FullContactException {
+    if (this.getConsentPurposes() == null || this.getConsentPurposes().isEmpty()) {
+      throw new FullContactException("missing/empty required consent field: consentPurposes");
+    } else {
+      for (PurposeRequest purposeRequest : this.getConsentPurposes()) {
+        purposeRequest.validate();
+      }
+    }
+    if (!isPopulated(this.getPolicyUrl())) {
+      throw new FullContactException("policyUrl is required for PermissionRequest");
+    }
+    if (!isPopulated(this.getTermsService())) {
+      throw new FullContactException("termsService is required for PermissionRequest");
+    }
+    if (!isPopulated(this.getCollectionMethod())) {
+      throw new FullContactException("collectionMethod is required for PermissionRequest");
+    }
+    if (!isPopulated(this.getCollectionLocation())) {
+      throw new FullContactException("collectionLocation is required for PermissionRequest");
+    }
+    if (!isPopulated(this.getIpAddress())) {
+      throw new FullContactException("ipAddress is required for PermissionRequest");
+    }
+  }
+}
