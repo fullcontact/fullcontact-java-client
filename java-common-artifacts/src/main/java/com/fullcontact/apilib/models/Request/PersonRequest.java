1c1e39f2aef28f993555c425a62ba04cecb5381a
Prakhar <47050738+prakhar-j@users.noreply.github.com>
2021-08-16 11:59:27 +0530

    Added comments for new fields

--- java-common-artifacts/src/main/java/com/fullcontact/apilib/models/Request/PersonRequest.java
+++ java-common-artifacts/src/main/java/com/fullcontact/apilib/models/Request/PersonRequest.java
@@ -17,6 +17,10 @@ public class PersonRequest extends MultifieldRequest {
   private Confidence confidence;
   @Builder.Default private boolean infer = true;
   private List<String> dataFilter;
-  private boolean verifiedPhysical, expandedInterests;
+  /** Return only profiles which are with verified physical address */
+  private boolean verifiedPhysical;
+  /** Temporary option to opt-in for more affinities data */
+  private boolean expandedInterests;
+  /** Limit the number of MAIDs in response */
   private Integer maxMaids;
 }
